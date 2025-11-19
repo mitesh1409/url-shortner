da1a44855cb3cb3653209e61db1537f8339348ce
Mitesh Prajapati <mitesh5.mprajapati@gmail.com>
2025-11-19 13:25:09 +0530

    refactor: Add a comment for list urls route

--- routes/urls.router.js
+++ routes/urls.router.js
@@ -4,6 +4,7 @@ import * as urlsController from '../controllers/urlsController.js';
 
 const urlsRouter = express.Router();
 
+// GET /urls - List all URLs
 urlsRouter.get('/', urlsController.listUrls);
 // GET /urls/:shortId - Redirect to original URL
 urlsRouter.get('/:shortId', urlsController.redirectToOriginalUrl);
