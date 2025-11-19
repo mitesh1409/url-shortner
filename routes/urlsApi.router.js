aee0846bc8e993c3877f336522c18dfffbe6371b
Mitesh Prajapati <mitesh5.mprajapati@gmail.com>
2025-11-19 12:16:35 +0530

    refactor: Add comments for the URL resource endpoints

--- routes/urlsApi.router.js
+++ routes/urlsApi.router.js
@@ -4,8 +4,11 @@ import * as urlApiController from '../controllers/urlApiController.js';
 
 const urlsApiRouter = express.Router();
 
+// POST /api/urls - Generate a short URL
 urlsApiRouter.post('/', urlApiController.generateShortUrl);
+// GET /api/urls/:shortId - Redirect to original URL
 urlsApiRouter.get('/:shortId', urlApiController.redirectToOriginalUrl);
+// GET /api/urls/analytics/:shortId - Get URL analytics
 urlsApiRouter.get('/analytics/:shortId', urlApiController.getUrlAnalytics);
 
 export {
