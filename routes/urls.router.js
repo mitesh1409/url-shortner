543ff13df0f19b1a8040031094c323b86a45675e
Mitesh Prajapati <mitesh5.mprajapati@gmail.com>
2025-11-20 10:31:10 +0530

    feature: Apply authenticate middleware

--- routes/urls.router.js
+++ routes/urls.router.js
@@ -1,17 +1,18 @@
 import express from 'express';
 
 import * as urlsController from '../controllers/urlsController.js';
+import { authenticate } from '../middlewares/authenticate.js';
 
 const urlsRouter = express.Router();
 
 // GET /urls - List all URLs
-urlsRouter.get('/', urlsController.listUrls);
+urlsRouter.get('/', authenticate, urlsController.listUrls);
 
 // GET /urls/generate - Show form to generate a new short URL
-urlsRouter.get('/generate', urlsController.showFormToGenerateShortUrl);
+urlsRouter.get('/generate', authenticate, urlsController.showFormToGenerateShortUrl);
 
 // POST /urls/generate - Handle form submission to generate a new short URL
-urlsRouter.post('/generate', urlsController.generateShortUrl);
+urlsRouter.post('/generate', authenticate, urlsController.generateShortUrl);
 
 // GET /urls/:shortId - Redirect to original URL
 urlsRouter.get('/:shortId', urlsController.redirectToOriginalUrl);
